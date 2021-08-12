a46a17e35bbf84f2f11a81cad3902e80f1087fff
PC <PC@TAKAI-XEON>
2021-08-11 22:14:05 -0300

    dfsdfs

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -14,12 +14,11 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("Olá ssssss!\nQual é o seu nome?"); 
 		// TODO Auto-generated method stub
 		// exemplo de comentario
 		System.out.println("Olá amigo!\nQual é o seu nome?");
 		Scanner oi = new Scanner(System.in);
-		
-	
 		String nome = oi.nextLine();
 		System.out.printf("Teste");
 		System.out.printf("Olá %s!\n", nome);
