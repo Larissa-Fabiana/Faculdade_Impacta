3380bb07699c56e1071cdad63412f39ca31f3d9a
Larissa-Fabiana <39524481+Larissa-Fabiana@users.noreply.github.com>
2021-08-11 21:06:09 -0300

    Update Principal.java
    Modified print on principal

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -21,7 +21,7 @@ public class Principal {
 		String nome = oi.nextLine();
 		System.out.printf("Teste");
 		System.out.printf("Olá %s!\n", nome);
-		System.out.printf("Exemplo de comentário");
+		System.out.printf("Exemplo de merge errado");
 	}
 
 }
